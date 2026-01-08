e186b21b404b8a4b62a326eb743cfb2462969f28
cheoljun99 <cheoljun99@naver.com>
2026-01-08 16:40:15 +0900

    solved(BOJ18111 / S2): 마인크래프트

---
+++ BOJ/26-1-W1/BOJ18111/cheoljun99.cpp
@@ -0,0 +1,73 @@
+﻿/*
+BOJ 18111 Four 마인크래프트 Silver 2
+
+3중 for문 부르트포스
+
+256 * 500 * 500 = 64,000,000
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+	int N,M,B;
+	cin >> N >> M >> B;
+	vector<vector<int>>map(N, vector<int>(M, 0));
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			cin >> map[i][j];
+		}
+	}
+	int answer = -1;
+	int high = -1;
+	for (int i = 0; i <= 256; ++i) {
+		int total_need = 0;
+		int total_erase = 0;
+		for (int x = 0; x < N; ++x) {
+			for (int y = 0; y < M; ++y) {
+				if (i < map[x][y]) {
+					total_erase += (map[x][y] - i);
+				}
+				else if (i > map[x][y]) {
+					total_need += (i - map[x][y]);
+				}
+			}
+		}
+		int temp = total_need - B;
+		if (temp == 0) {
+			if (answer == -1) {
+				answer = B + (2 * total_erase);
+				high = i;
+			}
+			else if (answer >= (B + (2 * total_erase))) {
+				answer = B + (2 * total_erase);
+				high = i;
+			}
+		}
+		else if (temp < 0) {
+			if (answer == -1) {
+				answer = B + temp + (2 * total_erase);
+				high = i;
+			}
+			else if (answer >= (B + temp + (2 * total_erase))) {
+				answer = B + temp + (2 * total_erase);
+				high = i;
+			}
+		}
+		else if (temp > 0) {
+			if (total_erase >= temp) {
+				if (answer == -1) {
+					answer = B + temp + (2 * total_erase);
+					high = i;
+				}
+				else if (answer >= (B + temp + (2 * total_erase))) {
+					answer = B + temp + (2 * total_erase);
+					high = i;
+				}
+			}
+		}
+	}
+	cout << answer<<" "<<high;
+	return 0;
+}
