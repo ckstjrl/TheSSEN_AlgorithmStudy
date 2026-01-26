b958cf031ec3e148c618f3ba27a95f7f608a21ba
cheoljun99 <cheoljun99@naver.com>
2026-01-26 15:17:15 +0900

    solved(BOJ2609 / B1): 최대공약수와 최소공배수

---
+++ BOJ/26-1-W4/BOJ2609/cheoljun99.cpp
@@ -0,0 +1,36 @@
+﻿// solved(BOJ2609 / B1): 최대공약수와 최소공배수
+// 유클리드 호제법
+
+#include <bits/stdc++.h>
+
+int gcd(int a, int b) {
+	int c = a % b;
+	while (c != 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b;
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int r1;
+	int r2;
+	if (a < b) {
+		r1 = gcd(b, a);
+		r2 = lcm(b, a);
+	}
+	else {
+		r1 = gcd(a, b);
+		r2 = lcm(a, b);
+	}
+	cout << r1 << '\n' << r2;
+	return 0;
+}
