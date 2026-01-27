8cd243f18d1a301465a9e759dba62db905232eb9
cheoljun99 <cheoljun99@naver.com>
2026-01-27 15:53:03 +0900

    solved(BOJ2798 / B2): 블랙잭

---
+++ BOJ/26-1-W4/BOJ2798/cheoljun99.cpp
@@ -0,0 +1,30 @@
+﻿// solved(BOJ2798 / B2): 블랙잭
+// 부르트포스
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+	int N, M;
+	cin >> N >> M;
+	vector<int> vec(N, 0);
+	for (int i = 0; i < N; ++i) {
+		cin >> vec[i];
+	}
+	int res = 0;
+	for (int i = 0; i < N; ++i) {
+		int a = vec[i];
+		for (int j = i + 1; j < N; ++j) {
+			int b = vec[j];
+			for (int k = j + 1; k < N; ++k) {
+				int c = vec[k];
+				int temp = a + b + c;
+				if (temp<= M&& temp >res) {
+					res = temp;
+				}
+			}
+		}
+	}
+	cout << res;
+	return 0;
+}
